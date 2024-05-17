bf6fcf8ba8a5b6f40c5143777a061ea239b66d79
EudesNilton <eudes258@gmail.com>
2024-05-17 13:44:55 -0300

    finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone c1 = new Nokia("223303", "Nokia 123", "128237c8", 390);
+c1.Ligar();
+c1.ReceberLigacao();
+c1.InstalarAplicativo("Instagram");
+Console.WriteLine();
+
+Smartphone c2 = new Iphone("223303", "Iphone 12a3", "128237c8", 390);
+c2.Ligar();
+c2.ReceberLigacao();
+c2.InstalarAplicativo("Instagram");
+Console.WriteLine();
